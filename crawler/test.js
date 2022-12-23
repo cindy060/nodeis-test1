9518f9dcbc91ba0b0e48434a0304c41ad0dce0a0
cindy <c0981048903@gmail.com>
2022-12-23 19:09:45 +0800

    new

--- crawler/test.js
+++ crawler/test.js
@@ -1,19 +1,23 @@
 // install mysql2
-
 const mysql2 = require('mysql2/promise');
 
+// npm i dotenv
+require('dotenv').config();
+// let dotenv = require('dotenv');
+// dotenv.config();
+
 (async () => {
+  console.log(process.env.DB_USER);
   const connection = await mysql2.createConnection({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'admin',
-    password: '',
-    database: 'stock_mfee31',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
   });
 
   // simple query
   let [data, fields] = await connection.query('SELECT * FROM `stocks`');
-//   let data = result[0];
   // console.log(result);
   console.log(data);
 
